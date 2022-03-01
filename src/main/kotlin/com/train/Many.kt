c98a38079f4256f297d75876b2ee75f98f9e77c6
wes0316 <wes0316@gmail.com>
2022-03-01 18:29:44 +0800

    0301

--- src/main/kotlin/com/train/Many.kt
+++ src/main/kotlin/com/train/Many.kt
@@ -1,8 +1,37 @@
 package com.train
 
+import java.time.LocalDateTime
+
 fun main(args: Array<String>) {
 
+    val parkinglot = HashMap<String,Car>();
+    var enter = LocalDateTime.of(2018,12,25,8,0,0)
+    var  car:Car? =Car("AAA-001",enter);
+    parkinglot.put(car!!.id,car);
+    car = Car("BBB-002",enter.plusMinutes(15));
+    parkinglot.put(car!!.id,car);
+    var leave = LocalDateTime.of(2018,12,25,9,0,0)
+
+    //car1 leaving
+    car = parkinglot.get("AAA-001");
+    car?.leave=leave;
+    println("${car?.id} duration: ${car?.duration()}")
+    parkinglot.remove("AAA-001");
+    println(parkinglot.size);
+
+    //car2 leaving
+    car =  parkinglot.get("BBB-002");
+    car?.leave = leave.plusHours(3);
+    println("${car?.id} duration is ${car?.duration()}")
+    parkinglot.remove("BBB-002");
+    println(parkinglot.size);
+
+
+
+
+
     //list()
+
 
 }
 
